ffef278485f89a430a086d2f12379d38356643dc
ElPepeMtz1981 <elpepemtz@gmail.com>
2025-08-25 15:06:45 -0600

    Adding Healt check

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { login } from './LoginService';
 import { useNavigate } from 'react-router-dom';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
@@ -6,7 +6,22 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 const Login = () => {
   const [form, setForm] = useState({ user: '', password: '' });
   const [error, setError] = useState('');
+  const [apiStatus, setApiStatus] = useState(null);
+
   const navigate = useNavigate();
+
+useEffect(() => {
+    fetch('http://localhost/AppointmentsAPI/api/health/ping')
+      .then(res => res.json())
+      .then(data => {
+        console.log('API OK:', data);
+        setApiStatus(data.status);
+      })
+      .catch(err => {
+        console.error('API no disponible:', err);
+        setApiStatus('ERROR');
+      });
+  }, []);
 
   const handleChange = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value });
@@ -24,7 +39,7 @@ const Login = () => {
   };
 
 return (
-    <div className="container d-flex justify-content-center align-items-center" style={{ minHeight: '100vh' }}>
+    <div className="container d-flex justify-content-center align-items-center" style={{ minHeight: '100vh' }}>      
       <div className="card p-4 shadow" style={{ maxWidth: '400px', width: '100%' }}>
         <h3 className="text-center mb-3">Iniciar sesión</h3>
         {error && <div className="alert alert-danger">{error}</div>}
@@ -53,6 +68,14 @@ return (
           </div>
           <button type="submit" className="btn btn-primary w-100">Entrar</button>
         </form>
+        <br />
+        {apiStatus === 'OK' ? (
+          <p style={{ color: 'green' }}>API funcionando ✅</p>
+        ) : apiStatus === 'ERROR' ? (
+          <p style={{ color: 'red' }}>Error de conexión con la API ❌</p>
+        ) : (
+          <p>Verificando conexión con la API...</p>
+      )}
       </div>
     </div>
   );
